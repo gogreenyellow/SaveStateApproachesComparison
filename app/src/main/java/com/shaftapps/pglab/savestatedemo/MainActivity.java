f5060bdae663d1e0a5793716211aeaf8da63a266
paulina_glab <ppglab@gmail.com>
2017-09-17 16:00:37 +0200

    Better UI

--- app/src/main/java/com/shaftapps/pglab/savestatedemo/MainActivity.java
+++ app/src/main/java/com/shaftapps/pglab/savestatedemo/MainActivity.java
@@ -3,7 +3,6 @@ package com.shaftapps.pglab.savestatedemo;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.CardView;
 import android.view.View;
 
 /**
@@ -14,34 +13,34 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String VISIBLE_KEY = "token";
 
-    private boolean mCardShown;
-    private CardView mCardView;
+    private boolean mSecretShown;
+    private View mSecretView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mCardView = (CardView) findViewById(R.id.am_card);
+        mSecretView = findViewById(R.id.am_secret_view);
 
         if (savedInstanceState != null && savedInstanceState.containsKey(VISIBLE_KEY)) {
             if (savedInstanceState.getBoolean(VISIBLE_KEY)) {
-                showCardView();
+                showSecretView();
             }
         }
     }
 
     public void handleButtonClick(View v) {
-        showCardView();
+        showSecretView();
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        outState.putBoolean(VISIBLE_KEY, mCardShown);
+        outState.putBoolean(VISIBLE_KEY, mSecretShown);
     }
 
-    private void showCardView() {
-        mCardShown = true;
-        mCardView.setVisibility(View.VISIBLE);
+    private void showSecretView() {
+        mSecretShown = true;
+        mSecretView.setVisibility(View.VISIBLE);
     }
 }
